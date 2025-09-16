723383c5070231c9bf707ce560e80011da269df4
clan-graziano <159022062+clan-graziano@users.noreply.github.com>
2025-09-16 09:51:52 +0000

    secondo commit

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 from flask import Flask
 from flask import send_file
 import random
+import json
+from markupsafe import escape
 
 app = Flask(__name__)
 
-
-
- 
 @app.route("/song")
-
 def song():
     return send_file('song.json')
 
-@app.route("/all")
-
-def index():
-    return send_file('song.json')
-
-@app.route("/")
-
-def random():
+@app.route("/rnd")
+def rnd():
     f = open('song.json') #Aprire il file json
     data = json.load(f) #caricare l'oggetto dentro data
-    return print(data['song'][random.randrange(0,2)]) #estrarre una riga a caso dal vettore data
-    
-# Checks to see if the name of the package is the run as the main package.
+    return data['song'][random.randrange(0,9)] #estrarre una riga a caso dal vettore data
+
+@app.route('/song/<artista>')
+def cerca_artista(artista):
+    f = open('song.json')
+    data = json.load(f)
+    for song in data['song']:
+        if song['artista'] == artista:
+            return song
 if __name__ == "__main__":
-    # Runs the Flask application only if the main.py file is being run.
     app.run()
